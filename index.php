4271b273e072a13f6aad014df711d9b8bb78e65c
valuest <zhoupeng_L@163.com>
2016-10-08 17:06:56 +0000

    second
    second

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php 
 //引用thinkphp框架制作药品进销存管理系统
+//开发模式
+define('APP_DEBUG',ture);
+//静态资源文件访问目录设置常量
+define('CSS_URL','/medpss/Public/css/');
+define('IMG_URL','/medpss/Public/images/');
+define('JS_URL','/medpss/Public/js/');
 
 //引入框架接口文件
 include('../ThinkPHP/ThinkPHP.php'); 
